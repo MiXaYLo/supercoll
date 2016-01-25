8ffcba4a5031fefb77fd157857375e7d289d5820
MiXaYLo <mehdinuruzade@std.sehir.edu.tr>
2016-01-25 18:34:29 +0400

    Test

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var MyBankAccount: Int = -500
+var MyAge: UInt = 22
+
+var BigNumber: Int64 = 14576576557656767
+
+var aNum = 6
+
+var Money: Double = 5.55
+
+var bVal: Float = 5.7
+
+ 
